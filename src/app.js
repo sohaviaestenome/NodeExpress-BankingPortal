78b607d4afefc91fcccd078e79ff6d08db0f021a
pacs <pacs@outlook.pt>
2022-03-02 16:47:11 +0000

    setting up express, missing index.ejs

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const { append } = require("express/lib/response");
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req,res) => {
+    res.render('index', {
+        title: 'Index'
+    });
+});
+
+
+
+app.listen(port,(req,res) => {
+    console.log("PS Project Running on port 3000!");
+})
