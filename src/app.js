b41fc284f4e17f729aa95d5ece8c08ab11cb0c42
pacs <pacs@outlook.pt>
2022-03-03 11:26:59 +0000

    created data library with errors

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 const accountData = fs.readFileSync('src/json/accounts.json','utf8');
 const accounts = JSON.parse(accountData);
 
-const userData = fs.readFileSync('src/json/accounts.json','utf8');
+const userData = fs.readFileSync('src/json/users.json','utf8');
 const users = JSON.parse(userData);
 
 
