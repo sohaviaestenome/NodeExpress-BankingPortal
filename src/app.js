cb767ff594c4349e829e3191409cbf070c4d12df
pacs <pacs@outlook.pt>
2022-03-03 11:25:03 +0000

    created data library with errors

--- src/app.js
+++ src/app.js
@@ -11,11 +11,41 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync('src/json/accounts.json','utf8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('src/json/accounts.json','utf8');
+const users = JSON.parse(userData);
+
+
+
 app.get('/', (req,res) => {
     res.render('index', {
-        title: 'Index'
+        title: 'Account Summary',
+        accounts: accounts
     });
 });
+
+app.get('/savings', (req,res)=> {
+    res.render('account', {
+        account:accounts.savings
+    });
+});
+
+app.get('/checking', (req,res)=>{
+    res.render('')
+})
+
+app.get('/credit', (req,res)=>{
+    res.render('')
+})
+
+app.get('/profile', (req,res)=>{
+    res.render('profile', {
+        user:users[0]
+    });
+});
+
 
 
 
