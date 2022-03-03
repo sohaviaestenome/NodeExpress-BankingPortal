eaa51853f2966ba3b1e556a58ed2b17917ba9e07
pacs <pacs@outlook.pt>
2022-03-03 13:22:04 +0000

    finished handling form data

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const { append } = require("express/lib/response");
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
+const { applyTo } = require("ramda");
 
 const app = express();
 const port = 3000;
@@ -59,13 +60,30 @@ app.get('/transfer',(req,res)=> {
     res.render('transfer');
 });
 
+app.post('/transfer', (req,res) => {
+    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount,10);
+    const accountsJSON = JSON.stringify(accounts,null,4);
+    fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,'utf8');
+    res.render('transfer', {message: 'Transfer Completed'});
+});
+
 app.get('/payment',(req,res)=>{
     res.render('payment', {
         account:accounts.credit
     });
 });
 
-
+app.post('/payment', (req,res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount,10);
+    const accountsJSON = JSON.stringify(accounts,null,4);
+    fs.writeFileSync(path.join(__dirname,'json','accounts.json'), accountsJSON,'utf8');
+    res.render('payment', {
+        message: 'Payment Successful',
+         account: accounts.credit
+        });
+});
 
 
 
